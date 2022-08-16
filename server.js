15c5d65e9e25707be0d5c467da749502caaad5d8
shetara2022 <smithshetarall@gmail.com>
2022-08-16 19:22:41 -0400

    created folders and file, sending index, show data

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+//CONFIGURATION 
+require('dotenv').config()
+const PORT = process.env.PORT
+console.log(PORT)
+const app = express()
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads!')
+})
+
+//BREADS
+const breadsController = require('./controllers/bread_controller.js')
+app.use('/breads', breadsController)
+
+
+//LISTEN
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+})
