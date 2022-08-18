e264fd76c790a43bf99d28d66832a24fa5d804a3
shetara2022 <smithshetarall@gmail.com>
2022-08-18 18:39:46 -0400

    included main.css and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -17,10 +17,15 @@ breads.get('/', (req, res) => {
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.render('Show', {
-        bread: Bread[req.params.arrayIndex]
-    })
+    if (Bread[req.params.arrayIndex]) {
+        res.render('Show', {
+            bread: Bread[req.params.arrayIndex]
+        })
+    } else {
+        res.send('404')
+    }
 })
+
 
 module.exports = breads
 
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 
 //ROUTES
@@ -21,6 +22,10 @@ app.get('/', (req, res) => {
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('Error 404: page not found!')
+})
 
 //LISTEN
 app.listen(PORT, () => {
