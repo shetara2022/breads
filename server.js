a4d0c85d555ef6f62103345231b0fd4397f69ecc
shetara2022 <smithshetarall@gmail.com>
2022-08-18 20:44:46 -0400

    create new.jsx, new form and buttons

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 
 //ROUTES
@@ -18,9 +19,11 @@ app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
 
+
 //BREADS
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
 
 // 404 Page
 app.get('*', (req, res) => {
