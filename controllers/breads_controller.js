8dd488bacc429e4d12a6d8f1c528192f96693200
shetara2022 <smithshetarall@gmail.com>
2022-08-21 11:19:08 -0400

    edit and update routes completed

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -18,6 +18,14 @@ breads.get('/new', (req, res) => {
     res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+        bread: Bread[req.params.indexArray],
+        index: req.params.indexArray
+    })
+})
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
@@ -42,6 +50,17 @@ breads.post('/', (req, res) => {
     }
     Bread.push(req.body)
     res.redirect('/breads')
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread[req.params.arrayIndex] = req.body
+    res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 
--- views/show.jsx
+++ views/show.jsx
@@ -21,7 +21,9 @@ function Show ({bread, index}) {
           <li><a href="/breads">Go home</a></li>
           <form action={`/breads/${index}?_method=DELETE`} method="POST">
     <input type='submit' value="DELETE" />
-  </form>
+          </form>
+          <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
 </Default>
       )
 }
